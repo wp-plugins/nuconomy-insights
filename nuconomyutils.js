bcef6234b8a9560ddf0d6a7bbcaba3cb05265590
yosit <yosit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-14 09:43:25 +0000

    Initial checkin for 1.24
    git-svn-id: https://plugins.svn.wordpress.org/nuconomy-insights/trunk@64770 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nuconomyutils.js
@@ -0,0 +1,15 @@
+﻿function hasNucRSSCookie(c_name)
+{
+  if (document.cookie.length>0)
+  {
+    c_start=document.cookie.indexOf(c_name + "=");
+    if (c_start!=-1)
+    {
+      document.cookie = c_name + "=; expires=Fri, 13 Jul 2001 05:28:21 UTC; path=/";
+      return true;
+    }
+  }
+  return false;
+}
+
+
