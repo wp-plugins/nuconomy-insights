7c48c90ff47da4c3ee5ea159f043af41bcf8ace9
yosit <yosit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-03 07:58:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/nuconomy-insights/tags/1.36@67296 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nuconomyutils.js
@@ -0,0 +1,26 @@
+﻿function hasNucRSSCookie(c_name)
+{
+  if (document.cookie.length>0)
+  {
+    c_start=document.cookie.indexOf(c_name + "=");
+    if (c_start!=-1)
+    {
+      document.cookie = c_name + "=; expires=Fri, 13 Jul 2001 05:28:21 UTC; path=/";
+      return true;
+    }
+  }
+  return false;
+}
+
+function fastswfembed(src,width,height,index) {
+  swfobject.embedSWF(src+'&enablejsapi=1&playerapiid=player'+index,
+                     'nuconyt'+index,
+                     width,
+                     height,
+                     "8",
+                     null,
+                     null, 
+                     { allowScriptAccess: "always" },
+                     { id: 'player'+index}
+                     );
+}
